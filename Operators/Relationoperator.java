d4b2fb2e88a43efde7a9e24eb1038cbc42ed41cd
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-13 00:23:23 +0530

    Relational operator

---
+++ Operators/Relationoperator.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class Relationoperator {
+    public static void main(String[] args) {
+
+        int var1 = 100;
+        int var2 = 300;
+
+        System.out.println("Is var1 = var2 " + (var1 == var2));
+        System.out.println("Is var1 > var2 " + (var1 > var2));
+        System.out.println("Is var1 < var2 " + (var1 < var2));
+        System.out.println("Is var1 >= var2 " + (var1 >= var2));
+        System.out.println("Is var1 <= var2 " + (var1 <= var2));
+        System.out.println("Is var1 not equal to var2 " + (var1 != var2));
+    }
+}
