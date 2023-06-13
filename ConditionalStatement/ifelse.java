d2bece0a81ccc4277cda13447c5425c8e058a0e6
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-13 15:37:10 +0530

    if else

--- ConditionalStatement/ifelse.java
+++ ConditionalStatement/ifelse.java
@@ -4,19 +4,15 @@ public class ifelse {
     public static void main(String[] args) {
         int x = 8;
 
-        if (x > 10 || x == 8) {
+        if (x > 10 || x == 8)
             System.out.println("Hello");
-        } else {
+        else
             System.out.println("Bye");
 
-        }
-
-        if (x > 10 && x == 8) {
+        if (x > 10 && x == 8)
             System.out.println("Hello");
-        } else {
+        else
             System.out.println("Bye");
-
-        }
 
     }
 }
