d8d71dc873d589ef0197e000f42d1f73aca141f6
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-13 13:11:06 +0530

    ternary operator

---
+++ Operators/ternaryoperator.java
@@ -0,0 +1,16 @@
+public class ternaryoperator {
+    public static void main(String[] args) {
+        String n = "Hi";
+
+        String result = n == "Hi" ? "yes" : "no";
+
+        System.out.println(result);
+
+        // to find even number
+        int n1 = 20;
+
+        int result1 = n1 % 2 == 0 ? 20 : 0;
+
+        System.out.println(result1);
+    }
+}
