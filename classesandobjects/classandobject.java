9652b9d73ee7cefe5eb99a3d45533f384e81fafd
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-14 13:23:08 +0530

    classes and objects

---
+++ classesandobjects/classandobject.java
@@ -0,0 +1,11 @@
+package classesandobjects;
+
+public class classandobject {
+    public static void main(String[] args) {
+        cal Cal = new cal(); // creating object
+        Cal.add(2, 100);
+
+        // System.out.println(result); Can use here alsoo
+    }
+
+}
