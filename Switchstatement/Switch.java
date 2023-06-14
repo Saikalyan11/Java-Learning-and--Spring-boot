2c985a3a8370468977761dbf0434aef788d779e3
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-14 13:02:15 +0530

    switch statement

---
+++ Switchstatement/Switch.java
@@ -0,0 +1,36 @@
+package Switchstatement;
+
+public class Switch {
+    public static void main(String[] args) {
+        int n = 3;
+
+        switch (n) {
+            case 1:
+                System.out.println("monday");
+                break;
+                case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+
+                break;
+
+            case 4:
+                System.out.println("thursday");
+                break;
+
+            case 5:
+                System.out.println("friday");
+                break;
+
+            case 6:
+                System.out.println("saturday");
+                break;
+
+
+            default:
+                break;
+        }
+    }
+}
