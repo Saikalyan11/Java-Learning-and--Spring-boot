90115cefec02157bdc4e47d147717072e4e92dc7
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-16 17:16:23 +0530

    Static and dynamic Arrays

---
+++ Arrays/Array.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+
+        // Static Array
+        int nums[] = { 1, 2, 3, 4, 5, 6, 7, 7, 8, 10, 12, 13 };
+
+        for (int i = 0; i <= nums.length; i++)
+            System.out.println(nums[i]);
+
+        // Dynamic Array
+
+        int numbers[] = new int[4];
+        numbers[0] = 20;
+        numbers[1] = 30;
+        numbers[2] = 50;
+        numbers[3] = 100;
+
+        for (int i = 0; i <= numbers.length; i++)
+            System.out.println(numbers[i]);
+
+    }
+}
