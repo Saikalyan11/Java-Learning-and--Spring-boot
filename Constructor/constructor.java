f7deb1e4deaa50520c294fd6810a03ecb39baa80
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-24 15:39:58 +0530

    Default and parameterized constructor

---
+++ Constructor/constructor.java
@@ -0,0 +1,25 @@
+package Constructor;
+
+public class constructor {
+    String name;
+    int id;
+    String batch;
+    String Interest;
+
+    public constructor() {
+
+    }
+
+    public constructor(String name, int id, String batch, String Interest) {
+        this.name = name;
+        this.id = id;
+        this.batch = batch;
+        this.Interest = Interest;
+    }
+
+    public String show() {
+        return name + " : " +
+                id + " : " + batch + " : "
+                + Interest;
+    }
+}
---
+++ Constructor/main.java
@@ -0,0 +1,12 @@
+package Constructor;
+
+public class main {
+    public static void main(String[] args) {
+        constructor cons = new constructor();
+        System.out.println("The default Constructor " + "'" + cons.show() + "'");
+        constructor conswithparameters = new constructor("Sai Kalyan",
+                132, "ECE",
+                "Computer Science");
+        System.out.println("The Parameterized Constructor " + "'" + conswithparameters.show() + "'");
+    }
+}
