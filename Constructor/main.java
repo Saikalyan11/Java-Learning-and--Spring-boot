ddd98ce1f80ed5017d45a30a0101078b8d4bdf8c
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-24 15:43:12 +0530

    Default and Parameterized constructorwith toString

--- Constructor/main.java
+++ Constructor/main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) {
         constructor cons = new constructor();
         System.out.println("The default Constructor " + "'" + cons.toString() + "'");
-        constructor conswithparameters = new constructor("Sai Kalyan",
+        constructor conswithparameters = new constructor("Sai Kalyan ",
                 132, "ECE",
                 "Computer Science");
         System.out.println("The Parameterized Constructor " + "'" + conswithparameters.toString() + "'");
