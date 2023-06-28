f516181661b31d546f544abc3d567b3cb8e90f94
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 18:39:51 +0530

    my custom design

--- PastternProgramming/Mycustom.java
+++ PastternProgramming/Mycustom.java
@@ -6,9 +6,9 @@ public class Mycustom {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (j == 0 || i == 0 || j == n || i == (n - 1) / 2) {
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                if (j == 0 || i == 0 || j == n - 1 || i == (n - 1) / 2) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
