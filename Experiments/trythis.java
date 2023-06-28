207e54a6e945fad26e222ecd4a2a4c56c6a688c5
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 13:38:52 +0530

    printing % *'s in a row and column

---
+++ Experiments/trythis.java
@@ -0,0 +1,14 @@
+package Experiments;
+
+public class trythis {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
