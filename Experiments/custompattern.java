c13b5fbd8bf6fd0d3cf79eea81ecf6b4dc583fd8
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 18:17:47 +0530

    Pattern design

---
+++ Experiments/custompattern.java
@@ -0,0 +1,20 @@
+package Experiments;
+
+import java.util.*;
+
+public class custompattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (j == 0 || j == (n - 1) / 2 || i == (n - 1) / 2 && j <= (n - 1) / 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
