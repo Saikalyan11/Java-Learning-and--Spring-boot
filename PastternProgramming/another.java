b87924ada843ea5b00d3f22f2704497649ff6da9
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 19:04:37 +0530

    another customized design pattern

---
+++ PastternProgramming/another.java
@@ -0,0 +1,26 @@
+package PastternProgramming;
+
+import java.util.*;
+
+import ConditionalStatement.elseif;
+
+public class another {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                if ((j == 0 && i > 0 && i < n - 1) || i == 0 && j > 0 && j <= (n - 1) / 2
+                        || i == (n - 1) && j > 0 && j <= (n - 1) / 2) {
+
+                    System.out.print("*");
+                }
+
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
