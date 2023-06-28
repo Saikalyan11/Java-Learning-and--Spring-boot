d60bec6bf3ea9da14f58372729402a3a7bff8ccd
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 21:30:54 +0530

    2 letters modified into 3 letters

--- PastternProgramming/twoletters.java
+++ PastternProgramming/twoletters.java
@@ -2,7 +2,7 @@ package PastternProgramming;
 
 import java.util.Scanner;
 
-public class twodigits {
+public class twoletters {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -22,6 +22,20 @@ public class twodigits {
             for (int j = 0; j <= n - 1; j++) {
                 if (j == 0 && i > 0 && i < n - 1 || i == 0 && j > 0 && j <= (n - 1) / 2
                         || i == (n - 1) && j > 0 && j <= (n - 1) / 2) {
+
+                    System.out.print("*");
+                }
+
+                else {
+                    System.out.print(" ");
+                }
+            }
+            // modifying
+            for (int j = 0; j <= n - 1; j++) {
+                if (j == (n - 1) / 4 && i < n - 1 && i > 0 ||
+                        j == (3 * n) / 4 && i < n - 1 && i > 0 ||
+                        i == 0 && j > (n - 1) / 4 && j < (3 * n) / 4 ||
+                        i == n - 1 && j > (n - 1) / 4 && j < (3 * n) / 4) {
 
                     System.out.print("*");
                 }
