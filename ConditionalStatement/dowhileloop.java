db66b79bc73f5cabc7d25f8dc2dd161076e839b0
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 13:07:29 +0530

    While and do while loops

---
+++ ConditionalStatement/dowhileloop.java
@@ -0,0 +1,11 @@
+package ConditionalStatement;
+
+public class dowhileloop {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("*");
+            i++;
+        } while (i < 100);
+    }
+}
---
+++ ConditionalStatement/whleloop.java
@@ -0,0 +1,13 @@
+package ConditionalStatement;
+
+public class whleloop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("*");
+
+            i++;
+        }
+
+    }
+}
