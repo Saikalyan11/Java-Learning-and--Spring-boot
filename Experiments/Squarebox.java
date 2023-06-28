bd87328c232ae4662081ee7ee5e11b5de540df13
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 16:03:13 +0530

    pattern programming

--- Experiments/Squarebox.java
+++ Experiments/Squarebox.java
@@ -2,8 +2,9 @@ package Experiments;
 
 public class Squarebox {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        int n = 6;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
                 if (i == 0 || i == 4 || j == 4 || j == 0) {
                     System.out.print("*");
                 } else {
