97ce7121c7da90036c09eda58170946ec6860554
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 18:18:27 +0530

    My custom design designed by myself

---
+++ Experiments/customdesign.java
@@ -0,0 +1,20 @@
+package Experiments;
+
+import java.util.*;
+
+public class customdesign {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (j == 0 || i == 0 || j == n || i == (n - 1) / 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
