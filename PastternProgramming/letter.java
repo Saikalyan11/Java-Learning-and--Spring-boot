64081221bed0f20772bf96af58d81b255e410f8f
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-28 21:49:25 +0530

    Designed a B letter using pattern programming

---
+++ PastternProgramming/letter.java
@@ -0,0 +1,27 @@
+package PastternProgramming;
+
+import java.util.*;
+
+public class letter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                if (j == 0 ||
+                        j == (n - 1) / 2 && i != 0 && i != (n - 1) / 2 && i != n - 1 ||
+                        i == 0 && j < (n - 1) / 2 ||
+                        i == (n - 1) / 2 && j < (n - 1) / 2 ||
+                        i == n - 1 && j < (n - 1) / 2) {
+
+                    System.out.print("*");
+                }
+
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
