2da8ba575e69dc52e15867fc592f0c8a9ef7db93
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-29 19:28:10 +0530

    2 Dagonals as Cross

--- PatternProgramming/Cross.java
+++ PatternProgramming/Cross.java
@@ -1,4 +1,21 @@
 package PatternProgramming;
 
+import java.util.Scanner;
+
 public class Cross {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i || i + j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+
+        }
+    }
 }
